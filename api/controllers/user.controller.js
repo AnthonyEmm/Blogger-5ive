26bba13ecda1b5766717c7a3092873e7cac6684d
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-01-31 18:25:03 +0100

    Full update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -11,6 +11,10 @@ export const updateUser = async (req, res, next) => {
     return next(errorHandler(403, "Not authorized to update user"));
   }
 
+  if (req.body.username && req.body.username.includes(" ")) {
+    return next(errorHandler(400, "Username cannot contain spaces"));
+  }
+
   if (req.body.password) {
     if (req.body.password.length < 6) {
       return next(errorHandler(400, "Password must be at least 6 characters"));
@@ -24,18 +28,16 @@ export const updateUser = async (req, res, next) => {
         errorHandler(400, "Username must be between 7 and 20 characters"),
       );
     }
-  }
-  if (req.body.username.includes(" ")) {
-    return next(errorHandler(400, "Username cannot contain spaces"));
-  }
-  if (req.body.username !== req.body.username.toLowerCase()) {
-    return next(errorHandler(400, "Username must be lowercase"));
-  }
 
-  if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-    return next(
-      errorHandler(400, "Username can only contain letters and numbers"),
-    );
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username can only contain letters and numbers"),
+      );
+    }
   }
 
   try {
