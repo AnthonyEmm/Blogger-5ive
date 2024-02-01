d5e4700224260f23e15a3066e371827b546c465e
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-02-01 11:59:16 +0100

    created get blogs API

--- api/controllers/blog.controller.js
+++ api/controllers/blog.controller.js
@@ -30,3 +30,48 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getblogs = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const blogs = await Blog.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { userId: req.query.slug }),
+      ...(req.query.blogId && { _Id: req.query.blogId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalBlogs = await Blog.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate(),
+    );
+
+    const lastMonthBlogs = await Blog.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      blogs,
+      totalBlogs,
+      lastMonthBlogs,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/blog.route.js
+++ api/routes/blog.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create } from "../controllers/blog.controller.js";
+import { create, getblogs } from "../controllers/blog.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.get("/getblogs", getblogs);
 
 export default router;
