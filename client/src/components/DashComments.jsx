54b70f3b3dfada2eb64e781724f3e621cd323181
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-02-07 21:55:41 +0100

    removed the delete user and comments functionality as an admin privilege

--- client/src/components/DashComments.jsx
+++ client/src/components/DashComments.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Table, Modal, Button } from "flowbite-react";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
-import { FaCheck, FaTimes } from "react-icons/fa";
-import { Tooltip } from "flowbite-react";
+import { Table } from "flowbite-react";
 
 const DashComments = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [comments, setComments] = useState([]);
   const [showMore, setShowMore] = useState(true);
-  const [showModal, setShowModal] = useState(false);
-  const [commentIdToDelete, setCommentIdToDelete] = useState("");
 
   useEffect(() => {
     const fetchComments = async () => {
@@ -54,29 +49,6 @@ const DashComments = () => {
     }
   };
 
-  const handleDeleteComment = async () => {
-    setShowModal(false);
-    try {
-      const res = await fetch(
-        `/api/comment/deleteComment/${commentIdToDelete}`,
-        {
-          method: "DELETE",
-        },
-      );
-      const data = await res.json();
-      if (res.ok) {
-        setComments((prev) =>
-          prev.filter((comment) => comment._id !== commentIdToDelete),
-        );
-        setShowModal(false);
-      } else {
-        console.log(data.message);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
   return (
     <div
       className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-600 scrollbar-thumb-slate-300
@@ -91,7 +63,6 @@ const DashComments = () => {
               <Table.HeadCell>Number of likes</Table.HeadCell>
               <Table.HeadCell>Blog ID</Table.HeadCell>
               <Table.HeadCell>User ID</Table.HeadCell>
-              <Table.HeadCell>Delete</Table.HeadCell>
             </Table.Head>
             {comments.map((comment) => (
               <Table.Body className="divide-y" key={comment._id}>
@@ -106,18 +77,6 @@ const DashComments = () => {
 
                   <Table.Cell>{comment.blogId}</Table.Cell>
                   <Table.Cell>{comment.userId}</Table.Cell>
-
-                  <Table.Cell>
-                    <span
-                      onClick={() => {
-                        setShowModal(true);
-                        setCommentIdToDelete(comment._id);
-                      }}
-                      className="text-red-500 font-medium hover:underline cursor-pointer"
-                    >
-                      Delete
-                    </span>
-                  </Table.Cell>
                 </Table.Row>
               </Table.Body>
             ))}
@@ -134,31 +93,6 @@ const DashComments = () => {
       ) : (
         <p>You have no comments yet!</p>
       )}
-
-      <Modal
-        show={showModal}
-        onClose={() => setShowModal(false)}
-        popup
-        size="md"
-      >
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-800 dark:text-gray-400">
-              Are you sure you want to delete this comment?
-            </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={handleDeleteComment}>
-                Yes, I'm sure
-              </Button>
-              <Button color="green" onClick={() => setShowModal(false)}>
-                No cancel
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
     </div>
   );
 };
--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Table, Modal, Button } from "flowbite-react";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Table } from "flowbite-react";
 import { FaCheck, FaTimes } from "react-icons/fa";
 import { Tooltip } from "flowbite-react";
 
@@ -10,8 +9,6 @@ const DashUsers = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [users, setUsers] = useState([]);
   const [showMore, setShowMore] = useState(true);
-  const [showModal, setShowModal] = useState(false);
-  const [userIdToDelete, setUserIdToDelete] = useState("");
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -52,23 +49,6 @@ const DashUsers = () => {
     }
   };
 
-  const handleDeleteUser = async () => {
-    try {
-      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
-        method: "DELETE",
-      });
-      const data = await res.json();
-      if (res.ok) {
-        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
-        setShowModal(false);
-      } else {
-        console.log(data.message);
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
   return (
     <div
       className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-600 scrollbar-thumb-slate-300
@@ -83,7 +63,6 @@ const DashUsers = () => {
               <Table.HeadCell>Username</Table.HeadCell>
               <Table.HeadCell>Email</Table.HeadCell>
               <Table.HeadCell>Admin</Table.HeadCell>
-              <Table.HeadCell>Delete</Table.HeadCell>
             </Table.Head>
             {users.map((user) => (
               <Table.Body className="divide-y" key={user._id}>
@@ -114,18 +93,6 @@ const DashUsers = () => {
                       </Tooltip>
                     )}
                   </Table.Cell>
-
-                  <Table.Cell>
-                    <span
-                      onClick={() => {
-                        setShowModal(true);
-                        setUserIdToDelete(user._id);
-                      }}
-                      className="text-red-500 font-medium hover:underline cursor-pointer"
-                    >
-                      Delete
-                    </span>
-                  </Table.Cell>
                 </Table.Row>
               </Table.Body>
             ))}
@@ -142,31 +109,6 @@ const DashUsers = () => {
       ) : (
         <p>You have no user yet!</p>
       )}
-
-      <Modal
-        show={showModal}
-        onClose={() => setShowModal(false)}
-        popup
-        size="md"
-      >
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-800 dark:text-gray-400">
-              Are you sure you want to delete this user?
-            </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={handleDeleteUser}>
-                Yes, I'm sure
-              </Button>
-              <Button color="green" onClick={() => setShowModal(false)}>
-                No cancel
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
     </div>
   );
 };
