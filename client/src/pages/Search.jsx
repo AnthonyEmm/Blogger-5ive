0957b2718f8c6d58306a1dbc6202268bb5ac6d74
Nnaemeka C. Emesowum <nna.emesowum@gmail.com>
2024-02-11 11:23:59 +0100

    add new option to search dropdown in search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -129,6 +129,7 @@ const Search = () => {
             >
               <option value="Uncategorized">Select a category</option>
               <option value="Beauty">Beauty</option>
+              <option value="Entertainment">Entertainment</option>
               <option value="Fashion">Fashion</option>
               <option value="Finance">Finance</option>
               <option value="Health">Health</option>
